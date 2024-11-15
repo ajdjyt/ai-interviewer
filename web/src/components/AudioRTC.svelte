5375392516e4cdab8c93b3e3161900ecf4ef83fe
ajdjyt <ajdjking@gmail.com>
2024-11-15 20:21:28 +0530

    basic ui

---
+++ web/src/components/AudioRTC.svelte
@@ -0,0 +1,12 @@
+<script>
+
+</script>
+
+<div class="flex flex-col min-h-screen min-w-screen mx-auto items-center justify-center bg-black text-slate-100">
+    <button class="min-w-[10rem] border border-indigo-700 rounded-[1rem] px-[1.2em] py-[0.6em]  m-6">
+        Start
+    </button>
+    <h1 id="output" class="min-w-[12rem] max-w-[12rem] min-h-[8rem] max-h-[8rem] text-center outline rounded-[1rem] px-[1.2em] py-[0.6em] outline-indigo-800 ">
+        wowza
+    </h1>
+</div>
